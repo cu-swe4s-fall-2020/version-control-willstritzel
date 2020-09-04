d8c762524210360bbe6cf57dc92a514204cf04c3
Will Stritzel <wist9668@colorado.edu>
2020-09-04 02:19:34 +0000

    -now I have used git add

--- math_lib.py
+++ math_lib.py
@@ -1,2 +1,19 @@
-def div(a, b):
-    return a/b
+import sys 
+op = sys.argv[1]
+a = int(sys.argv[2])
+b = int(sys.argv[3])
+
+if op == 'add':
+    print (a + b)
+elif op == 'sub':
+    print (a - b)
+elif op == 'mult':
+    print (a * b)
+elif op == 'div':
+    if b != 0:
+        print (a / b)
+    else:
+        print("Nice try cowboy")
+else:
+    print ("operator not recognized")
+
